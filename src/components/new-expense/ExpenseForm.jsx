9769afb3d404e290ce886207e16212dfda5ef41c
Bekbolsun <bekbolsun.azamatov.1579.7@gmail.com>
2023-07-12 12:39:03 +0600

    some changes

--- src/components/new-expense/ExpenseForm.jsx
+++ src/components/new-expense/ExpenseForm.jsx
@@ -4,49 +4,47 @@ import Button from '../UI/button/Button'
 import './ExpenseForm.css'
 
 const ExpenseForm = ({ onCloseForm, onAddNewExpense }) => {
-	// const [title, setTitle] = useState('')
-	// const [amount, setAmount] = useState('')
-	// const [date, setDate] = useState('')
+	const [title, setTitle] = useState('')
+	const [amount, setAmount] = useState('')
+	const [date, setDate] = useState('')
 
-	// const isFormValid =
-	// 	title.trim().length !== 0 &&
-	// 	amount.trim().length !== 0 &&
-	// 	date.trim().length !== 0
+	const isFormValid =
+		title.trim().length !== 0 &&
+		amount.trim().length !== 0 &&
+		date.trim().length !== 0
 
-	// const titleChangeHandler = (e) => {
-	// 	setTitle(e.target.value)
-	// }
+	const titleChangeHandler = (e) => {
+		setTitle(e.target.value)
+	}
 
-	// const amountChangeHandler = (e) => {
-	// 	setAmount(e.target.value)
-	// }
+	const amountChangeHandler = (e) => {
+		setAmount(e.target.value)
+	}
 
-	// const dateChangeHandler = (e) => {
-	// 	setDate(e.target.value)
-	// }
+	const dateChangeHandler = (e) => {
+		setDate(e.target.value)
+	}
 
-	const titleRef = useRef()
+	// const titleRef = useRef()
 
 	const submitHandler = (e) => {
 		e.preventDefault()
-		// if (isFormValid) {
-		// 	const expense = {
-		// 		date: new Date(date),
-		// 		title,
-		// 		amount: Number(amount),
-		// 		id: Date.now().toString(),
-		// 	}
-		// 	onAddNewExpense(expense)
+		if (isFormValid) {
+			const expense = {
+				date: new Date(date),
+				title,
+				amount: Number(amount),
+				id: Date.now().toString(),
+			}
+			onAddNewExpense(expense)
 
-		// 	setTitle('')
-		// 	setAmount('')
-		// 	setDate('')
-		// 	// state batching => пакетирование setState
-		// } else {
-		// 	alert('Заполните все поля!')
-		// }
-
-		titleRef.current.value = ''
+			setTitle('')
+			setAmount('')
+			setDate('')
+			// state batching => пакетирование setState
+		} else {
+			alert('Заполните все поля!')
+		}
 	}
 
 	return (
@@ -55,21 +53,21 @@ const ExpenseForm = ({ onCloseForm, onAddNewExpense }) => {
 				<input
 					label={'Заголовок'}
 					type='text'
-					// onChange={titleChangeHandler}
-					// value={title}
-					ref={titleRef}
+					onChange={titleChangeHandler}
+					value={title}
+					// ref={titleRef}
 				/>
 				<FormInput
 					label={'Количество'}
 					inputType='number'
-					// value={amount}
-					// onChange={amountChangeHandler}
+					value={amount}
+					onChange={amountChangeHandler}
 				/>
 				<FormInput
 					label={'Датировать'}
 					inputType='date'
-					// onChange={dateChangeHandler}
-					// value={date}
+					onChange={dateChangeHandler}
+					value={date}
 				/>
 			</div>
 			<div className='new-expense__actions'>
